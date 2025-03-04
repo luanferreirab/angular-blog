1c5dee47464fc4cd7e2715e5ba67344d9c254bd7
Luan Ferreira <luanbarroso01@hotmail.com.br>
2025-03-04 19:58:49 -0300

    components dinamycs propertys and responsive layout

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
+  photoCover: string = 'https://tm.ibxk.com.br/2022/11/21/21225117851006.jpg?ims=750x';
+  cardTitle: string = 'Saiu o novo jogo assasins creed valhalla';
+  cardDescription: string = 'Sinopse. O movimento do Grande Exército Pagão na Inglaterra durante as expansoes Vikings. O mapa mostra alguns dos reinos que o jogador pode explorar em Valhalla. Em Assassins Creed Valhalla, a história acontece perto do final do século IX, em 873, durante as Invasões Vikings da Grã Bretanha.'
 
   constructor() { }
 
