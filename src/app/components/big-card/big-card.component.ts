b54675c456c449b10dff3428cb1ae26f396f02cc
Luan Ferreira <luanbarroso01@hotmail.com.br>
2025-03-04 22:43:04 -0300

    content created

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
-  photoCover: string = 'https://tm.ibxk.com.br/2022/11/21/21225117851006.jpg?ims=750x';
-  cardTitle: string = 'Saiu o novo jogo assasins creed valhalla';
-  cardDescription: string = 'Sinopse. O movimento do Grande Exército Pagão na Inglaterra durante as expansoes Vikings. O mapa mostra alguns dos reinos que o jogador pode explorar em Valhalla. Em Assassins Creed Valhalla, a história acontece perto do final do século IX, em 873, durante as Invasões Vikings da Grã Bretanha.'
+  @Input()
+  photoCover: string = '';
+  @Input()
+  cardTitle: string = '';
+  @Input()
+  cardDescription: string = ''
+  @Input()
+  Id: string = '0'
 
   constructor() { }
 
---
+++ src/app/pages/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        'id': '1',
+        'title': 'Saiu o novo jogo assasins creed valhalla',
+        'description': 'Sinopse. O movimento do Grande Exército Pagão na Inglaterra durante as expansoes Vikings. O mapa mostra alguns dos reinos que o jogador pode explorar em Valhalla. Em Assassins Creed Valhalla, a história acontece perto do final do século IX, em 873, durante as Invasões Vikings da Grã Bretanha.',
+        'photoCover': 'https://tm.ibxk.com.br/2022/11/21/21225117851006.jpg?ims=750x'
+    },
+    {
+        'id': '2',
+        'title': 'Avatar Frontiers of Pandora',
+        'description': 'Avatar: Frontiers of Pandora™ é um jogo de ação e aventura em primeira pessoa ambientado no mundo aberto da desconhecida Fronteira Ocidental de Pandora. A corporação militarista humana conhecida como RDA abduziu, treinou e moldou você, um Navi, para servir a ela.',
+        'photoCover': 'https://image.api.playstation.com/vulcan/ap/rnd/202306/0113/dff5e6c6b9cdd36b417d13aea5bc7cede609f70a44905ac8.jpg'
+    },
+    {
+        'id': '3',
+        'title': 'Tom Clancys The Division',
+        'description': 'Tom Clancys The Division é uma experiência revolucionária que combina a personalização robusta dos RPGs com um combate tático cheio de ação. Nunca houve um melhor momento para se juntar à luta e salvar a cidade de Nova York.',
+        'photoCover': 'https://s2-techtudo.glbimg.com/Nn_bCaF7U_n97ZOotut1prAMwjc=/0x0:695x391/984x0/smart/filters:strip_icc()/s.glbimg.com/po/tt2/f/original/2016/06/07/tom-clancys-the-divisiontm_20160310005913.jpg'
+    },
+    {
+        'id': '4',
+        'title': 'Farcry 5',
+        'description': 'O jogo é ambientado na fictícia Hope Country, Montana, Estados Unidos, onde um pregador cristão chamado Joseph Seed alcançou proeminência. Seed acredita que ele foi escolhido para proteger o povo de Hope County de um "colapso inevitável" e estabeleceu uma congregação chamada Projeto do Portão do Éden.',
+        'photoCover': 'https://i0.wp.com/dropsdejogos.uai.com.br/wp-content/uploads/sites/10/2020/05/far-cry-5-scaled.jpg?fit=960%2C569&ssl=1&resize=1280%2C720'
+    }
+]
